b4b75111e0d826d96e40e84a4a2615f1cf304f98
31edgar <3eesparza@gmail.com>
2025-02-27 10:41:55 +0100

    (Camping.java) - getters afegits

--- src/prog2/model/Camping.java
+++ src/prog2/model/Camping.java
@@ -1,9 +1,45 @@
 package prog2.model;
 import prog2.model.InCamping;
+import java.util.ArrayList;
 
 public class Camping implements InCamping {
+    // Atributs
     String nom;
-    // Llista d'allotjaments (amb tots els disponibles)
-    // Llista de clients
-    // Llista de reserves
+    ArrayList<Allotjament> llistaAllotjaments = new ArrayList<>();
+    ArrayList<Client> llistaClients = new ArrayList<>();
+    LlistaReserves llistaReserves = new LlistaReserves(); // CAL IMPLEMENTAR LA CLASSE LlistaReserves
+
+    // Mètodes
+    // - Getters
+    public String getNom() {
+        return nom;
+    }
+
+    public LlistaReserves getLlistaReserves() {
+        return llistaReserves;
+    }
+
+    public ArrayList<Allotjament> getLlistaAllotjaments() {
+        return llistaAllotjaments;
+    }
+
+    public ArrayList<Client> getLlistaClients() {
+        return llistaClients;
+    }
+
+    public int getNumAllotjaments() {
+        return llistaAllotjaments.size();
+    }
+
+    public int getNumReserves() {
+        // CAL IMPLEMENTAR LA CLASSE LlistaReserves
+        return 0;
+    }
+
+    public int getNumClients() {
+        return llistaClients.size();
+    }
+
+    // Mètodes per afegir
+
 }
